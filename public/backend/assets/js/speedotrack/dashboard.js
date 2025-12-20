70751c168e27b2d25983360106f2a88fcd83e402
balibalbal <balibalbal@gmail.com>
2025-12-20 12:04:23 +0700

    update dashboard

--- public/backend/assets/js/speedotrack/dashboard.js
+++ public/backend/assets/js/speedotrack/dashboard.js
@@ -243,9 +243,9 @@ function createMarkerPopup(d) {
 
 function selectRouteDeviceFromMap(imei) {
     // Tutup popup
-    if (markers[imei]) {
-        markers[imei].closePopup();
-    }
+    // if (markers[imei]) {
+    //     markers[imei].closePopup();
+    // }
 
     // Redirect ke histories index + kirim imei
     window.location.href = `/histories?imei=${encodeURIComponent(imei)}`;
