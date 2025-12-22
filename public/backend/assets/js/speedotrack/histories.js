8f6f38314da077650d43a5e0aaed3918268645be
balibalbal <balibalbal@gmail.com>
2025-12-22 18:44:27 +0700

    update history

--- public/backend/assets/js/speedotrack/histories.js
+++ public/backend/assets/js/speedotrack/histories.js
@@ -80,6 +80,36 @@ function initSpeedChart() {
     speedData = routeMeta.map(m => m.speed);
     speedLabels = routeMeta.map((_, i) => i + 1);
 
+    const glowPointPlugin = {
+        id: 'glowPoint',
+        afterDatasetDraw(chart, args) {
+            const { ctx } = chart;
+            const active = chart.getActiveElements();
+
+            if (!active.length) return;
+
+            const { datasetIndex, index } = active[0];
+            const meta = chart.getDatasetMeta(datasetIndex);
+            const point = meta.data[index];
+
+            if (!point) return;
+
+            ctx.save();
+
+            // ✨ glow effect
+            ctx.beginPath();
+            ctx.arc(point.x, point.y, 6, 0, Math.PI * 2);
+
+            ctx.fillStyle = '#dc3545';       // merah
+            ctx.shadowColor = '#dc3545';
+            ctx.shadowBlur = 15;
+            ctx.fill();
+
+            ctx.restore();
+        }
+    };
+
+
     speedChart = new Chart(canvas, {
         type: 'line',
         data: {
@@ -89,7 +119,7 @@ function initSpeedChart() {
                 borderWidth: 2,
                 tension: 0.3,
 
-                pointRadius: 5,
+                pointRadius: 0,
                 pointHoverRadius: 0,
 
                 borderColor: '#0d6efd'
@@ -106,7 +136,8 @@ function initSpeedChart() {
             plugins: {
                 legend: { display: false }
             }
-        }
+        },
+        plugins: [glowPointPlugin]
     });
 
     canvas.addEventListener('mousedown', e => {
@@ -338,30 +369,31 @@ function updateInfo(meta, index) {
     `;
 }
 
-// function highlightChart(index) {
-//     if (!speedChart) return;
-
-//     speedChart.setActiveElements([
-//         { datasetIndex: 0, index }
-//     ]);
-//     speedChart.update('none');
-// }
-
 function highlightChart(index) {
     if (!speedChart) return;
 
     speedChart.setActiveElements([
         { datasetIndex: 0, index }
     ]);
-
-    // 🔥 paksa redraw point aktif
-    speedChart.tooltip.setActiveElements(
-        [{ datasetIndex: 0, index }],
-        { x: 0, y: 0 }
-    );
-
     speedChart.update('none');
 }
+
+
+// function highlightChart(index) {
+//     if (!speedChart) return;
+
+//     speedChart.setActiveElements([
+//         { datasetIndex: 0, index }
+//     ]);
+
+//     // 🔥 paksa redraw point aktif
+//     speedChart.tooltip.setActiveElements(
+//         [{ datasetIndex: 0, index }],
+//         { x: 0, y: 0 }
+//     );
+
+//     speedChart.update('none');
+// }
 
 
 /* =========================
